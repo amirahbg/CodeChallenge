c3b1c43b2eb8634bbd135459441e7e431a9ec822
amir.bagheri <amirahbg@gmail.com>
2022-12-09 08:34:32 +0330

    add view binding library

---
+++ app/src/main/java/com/example/bonialcodechallenge/main/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.bonialcodechallenge.main
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.example.bonialcodechallenge.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
